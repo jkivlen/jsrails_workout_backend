6ffb2a3cb148b1b92bc8afba873cfc9943146f9c
jkivlen <jimmykivlen@gmail.com>
2021-05-01 21:38:46 -0500

    added index and create controllers

--- app/controllers/api/v1/workout_controller.rb
+++ app/controllers/api/v1/workout_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::WorkoutsController < ApplicationController
+
+    def index
+        @workouts = Workout.all
+        render json: @workouts
+    end
+
+    def create
+        workout = Workout.new(workout_params)
+            if workout.save
+                render json: workout, status: :accepted
+            else
+                render json: {errors: workout.errors.full_messages}, status: :unprocessible_entity
+            end
+    end
+
+    private
+
+    def workout_params
+        params.require(:workout).permit(:title, :description, :category_id)
+    end
+
 end
